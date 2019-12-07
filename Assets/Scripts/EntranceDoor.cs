9ff3abc60d83cbf2497094569bc7d3875af7717c
Oompa man <ben@nackfamily.com>
2019-12-06 19:01:42 -0600

    Added Entrance on level 01 and exit on level 00

---
+++ Assets/Scripts/EntranceDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntranceDoor : MonoBehaviour
+{
+    public GameObject doorOpen;
+    public GameObject doorClosed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        doorClosed.SetActive(false);
+        doorOpen.SetActive(true);
+    }
+}
